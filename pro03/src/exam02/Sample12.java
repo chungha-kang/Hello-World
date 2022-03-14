a903faaacd8ad9a960a86bf69c0ca4f715f33ad1
haeng <haeng@localhost>
2022-03-14 09:48:46 +0900

    왜 git 안에 pro04 생성이 안될까

--- pro03/src/exam02/Sample12.java
+++ pro03/src/exam02/Sample12.java
@@ -1,8 +1,36 @@
 package exam02;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Sample12 {
 
 	public static void main(String[] args) {
+		// 내 풀이
+		
+		
+		
+		// 강사님 풀이
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+		
+		String name, player, sCom = "";
+		int computer;
+		int result = 0;		// Win 1, Lose -1, Draw 0
+		int wCnt = 0, dCnt = 0, lCnt = 0;
+		
+		System.out.print("당신의 이름을 입력하시오 : ");
+		name = sc.nextLine();
+		
+		for(;;) {
+			System.out.print("가위 바위 보 입력 : ");
+			player = sc.nextLine();
+			computer = random.nextLine();
+			
+			if(computer == 0) {
+				
+			}
+		}
 		
 
 	}
