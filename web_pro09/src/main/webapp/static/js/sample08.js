fda634f8aeedefa07136b1d281586ef544e97250
user.email <Chung@DESKTOP-JFL4FV9>
2022-06-16 15:26:02 +0900

    6/16

--- web_pro09/src/main/webapp/static/js/sample08.js
+++ web_pro09/src/main/webapp/static/js/sample08.js
@@ -1,3 +1,19 @@
 /**
  * 
  */
+ window.onload = function() {
+	// .clone(true) 에 대한 이벤트 핸드러 복제는 JQuery 로 연결된 
+	// 이벤트 핸들러에 대해서만 적용 된다. 기존 DOM 의 addEventListener 
+	// 로 등록된 이벤트 핸들러는 동작하지 않는다.
+	
+ 	$(".copy1").bind("mouseover", function() {
+		this.style.color = 'red';
+	});
+	$(".copy1").bind("mouseout", function() {
+		this.style.color = 'black';
+	});
+
+	$("input").bind("input", function() {
+		console.log(this.value);
+	});
+}
