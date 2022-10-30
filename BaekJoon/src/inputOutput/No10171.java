ffebf30cc3d454c18427d8a7dc6162aa1b282a8a
user.email <cubone89@gmail.com>
2022-10-30 23:13:46 +0900

    백준 10171번

---
+++ BaekJoon/src/inputOutput/No10171.java
@@ -0,0 +1,10 @@
+package inputOutput;
+
+public class No10171 {
+
+	public static void main(String[] args) {
+		System.out.println(\);
+		System.out.println( A * (B.charAt(0) - '0'));
+		System.out.println( A * Integer.parseInt(B));
+	}
+}
