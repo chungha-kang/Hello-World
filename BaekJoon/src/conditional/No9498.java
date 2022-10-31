667dae0db08e5f4fce52e943190a7a7afbc755f1
user.email <cubone89@gmail.com>
2022-10-31 21:14:33 +0900

    백준 문제풀이(10/31)

---
+++ BaekJoon/src/conditional/No9498.java
@@ -0,0 +1,14 @@
+package conditional;
+
+import java.util.Scanner;
+
+public class No9498 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		
+		System.out.println();
+	}
+}
