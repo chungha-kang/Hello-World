84b03dbcfb636ff56623987890eb36ea693448ee
user.email <cubone89@gmail.com>
2022-12-03 17:52:18 +0900

    백준 풀이(12/03)

---
+++ BaekJoon/src/loop/No8393.java
@@ -0,0 +1,16 @@
+package loop;
+
+import java.util.Scanner;
+
+public class No8393 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);	
+		int a = in.nextInt();
+		
+		for(int i = 1; i < 10; i++) {
+			System.out.println(a + " * " + i + " = " + (a * i));
+		}
+	}
+}
