258c20acb75d029a3b4fab64faf4645ea54fdc3c
user.email <cubone89@gmail.com>
2022-12-28 11:38:35 +0900

    12/28 백준 풀이

---
+++ BaekJoon/src/array/No10807.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class No10807 {
+	
+	public static void main(String args[]) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int a = in.nextInt();
+		int arr[] = new int[a];
+		int cnt = 0;
+ 
+		for (int i = 0; i < a; i++) {
+			arr[i] = in.nextInt();
+		}
+		int v = in.nextInt();
+		
+		
+		for (int j=0; j < a; j++) {
+			if(arr[j] == v)
+				cnt++;
+		}
+		System.out.println(cnt);
+	}
+ 
+}
