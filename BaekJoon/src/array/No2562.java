3ffc784e64775dea69f7a0f09f62d91681835e34
user.email <cubone89@gmail.com>
2023-01-02 22:41:37 +0900

    백준 문제풀이(01/02)

---
+++ BaekJoon/src/array/No2562.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.Scanner;
+
+public class No2562 {
+	
+	public static void main(String args[]) {
+ 
+		Scanner in = new Scanner(System.in);
+		int[] arr = { in.nextInt(), in.nextInt(), in.nextInt(),
+				in.nextInt(), in.nextInt(), in.nextInt(),
+				in.nextInt(), in.nextInt(), in.nextInt() };
+		in.close();
+		
+		int count = 0;
+		int max = 0;
+		int index = 0;
+        
+		for(int value : arr) {
+			count++;
+            
+			if(value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		System.out.print(max + "\n" + index);
+		
+	}
+}
