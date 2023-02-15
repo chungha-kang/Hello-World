883af205e16d1dceb7c465a7e0c08a4d352c7460
user.email <cubone89@gmail.com>
2023-02-15 18:31:48 +0900

    백준문제풀이(02/15)

---
+++ BaekJoon/src/second/inputOutput/No10430.java
@@ -0,0 +1,22 @@
+package second.inputOutput;
+
+import java.util.Scanner;
+
+public class No10430 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		System.out.println( (A+B)%C );
+		System.out.println( ((A%B) + (B%C))%C );
+		System.out.println( (A*B)%C );
+		System.out.println( (A%C) * (B%C)%C );
+		sc.close();
+	}
+	
+}
