0dd51ea03c66fbce0a55df3471c9124f1bfb996a
user.email <cubone89@gmail.com>
2023-02-20 18:32:21 +0900

    백준문제풀이(02/20)

---
+++ BaekJoon/src/second/inputOutput/No25083.java
@@ -0,0 +1,15 @@
+package second.inputOutput;
+
+public class No25083 {
+
+	public static void main(String[] args) {
+		String s = "         ,r'\"7\n" +
+				   "r`-_   ,'  ,/\n" +
+				   " \\. \". L_r'\n" +
+				   "   `~\\/\n" +
+				   "      |\n" +
+				   "      |";
+
+		System.out.print(s);
+	}
+}
