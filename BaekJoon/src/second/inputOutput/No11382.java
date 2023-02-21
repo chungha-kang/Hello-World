507c7af3d2605059995768185c11ad57ed64468e
user.email <cubone89@gmail.com>
2023-02-21 23:37:04 +0900

    백준문제풀이(02/21)

---
+++ BaekJoon/src/second/inputOutput/No11382.java
@@ -0,0 +1,15 @@
+package second.inputOutput;
+
+import java.util.Scanner;
+
+public class No11382 {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+        System.out.println(a+b+c);
+    }
+}
