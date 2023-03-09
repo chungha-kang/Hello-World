4f4582f032a07df53cbd1e2686d107c158a7c653
chungha-kang <cubone89@gmail.com>
2023-03-09 22:14:45 +0900

    백준 문제 풀이(23-03-09)

---
+++ BaekJoon/src/second/conditional/No1330.java
@@ -0,0 +1,21 @@
+package second.conditional;
+
+import java.util.Scanner;
+
+public class No1330 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        sc.close();
+        
+        if (A > B) {
+            System.out.println('>');
+        } else if (A < B) {
+            System.out.println('<');
+        } else if (A == B) {
+            System.out.println("==");
+        }
+	}
+}
